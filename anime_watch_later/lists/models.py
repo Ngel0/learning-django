1dea04f69f0cf42fbc3e808a1fda70a7a99e1ac0
Oleg Pak <pak.o99@mail.ru>
2023-06-17 03:45:31 +0300

    moved the models to a new startapp lists

---
+++ anime_watch_later/lists/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=30, verbose_name='название жанра')
+
+class Entry(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    medium_type = models.CharField(max_length=10, verbose_name='Вид медиа') #тип медиа: фильм сериал аниме книга другое
+    #genres = models.ManyToManyField(Genre, verbose_name='жанры', related_name='genre_books')
+    priority = models.IntegerField(default=0, verbose_name='Приоритет')
+    description = models.CharField(max_length=500, verbose_name='Описание')
+
