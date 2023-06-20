b2ce42605453199bd192fb428be4b258d6cfdde3
Oleg Pak <pak.o99@mail.ru>
2023-06-21 00:31:29 +0300

    added layout, added and learning bootstrap

--- anime_watch_later/website/views.py
+++ anime_watch_later/website/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import AnimeEntryForm
+from lists.models import Entry
 
 
 # Create your views here.
 
 def welcome(request):
-    submit_button = request.POST.get("submit")
-    anime_name = ''
-    anime_description = ''
+    return render(request, 'website/welcome.html',
+                  {"my_list": Entry.objects.all(),
+                  "count_entries": Entry.objects.count()})
 
-    form = AnimeEntryForm(request.POST or None)
-    if form.is_valid():
-        anime_name = form.cleaned_data.get("name")
-        anime_description = form.cleaned_data.get("description")
-
-    context = {'form': form, 'anime_name': anime_name,
-               'anime_description': anime_description, 'submit_button': submit_button}
-
-    return render(request, 'website/welcome.html', context)
+    # submit_button = request.POST.get("submit")
+    # anime_name = ''
+    # anime_description = ''
+    #
+    # form = AnimeEntryForm(request.POST or None)
+    # if form.is_valid():
+    #     anime_name = form.cleaned_data.get("name")
+    #     anime_description = form.cleaned_data.get("description")
+    #
+    # context = {'form': form, 'anime_name': anime_name,
+    #            'anime_description': anime_description, 'submit_button': submit_button}
+    #
+    # return render(request, 'website/welcome.html', context)
