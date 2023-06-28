292aa29a1d9c833c615267c73d5958403c0c7026
Oleg Pak <pak.o99@mail.ru>
2023-06-28 05:45:40 +0300

    added basic top nav bar

--- anime_watch_later/anime_watch_later/urls.py
+++ anime_watch_later/anime_watch_later/urls.py
@@ -14,13 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from website.views import welcome
 from lists.views import add, delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome),
+    path('', welcome, name='welcome'),
     path('add', add),
-    path('delete/<int:id>', delete)
+    path('delete/<int:id>', delete),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# Urls provided by django.contrib.auth.urls
+# accounts/ login/ [name='login']
+# accounts/ logout/ [name='logout']
+# accounts/ password_change/ [name='password_change']
+# accounts/ password_change/done/ [name='password_change_done']
+# accounts/ password_reset/ [name='password_reset']
+# accounts/ password_reset/done/ [name='password_reset_done']
+# accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/ reset/done/ [name='password_reset_complete']
