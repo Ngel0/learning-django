3026b734182b6a603868fe7e1ab0d1991cc82e2a
Oleg Pak <pak.o99@mail.ru>
2023-07-27 20:19:57 +0300

    added welcome page for unauthorized

--- anime_watch_later/website/views.py
+++ anime_watch_later/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from lists.models import Entry
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 # Create your views here.
@@ -8,17 +9,13 @@ def welcome(request):
     current_user = request.user
     if current_user.is_authenticated:
         current_entries = Entry.objects.filter(user=current_user.id)
-        # переименовать в list.html
-        return render(request, 'website/welcome.html',
+        return render(request, 'website/list.html',
                       {'my_list': current_entries,
                       'count_entries': current_entries.count()})
     else:
-        # пока что показывает всех для анонимного юзера
-        # сделать welcome страницу для неавторизованных
-        return render(request, 'website/welcome.html',
-                      {'my_list': Entry.objects.all(),
-                       'count_entries': Entry.objects.count()})
+        return render(request, 'website/welcome.html')
 
+@login_required
 def delete(request, id):
     try:
         entry = Entry.objects.get(pk=id)
